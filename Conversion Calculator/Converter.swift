c21ae48d19bb3df07946c180673895faa83cbf0c
Andy Hine <andyhine@Andys-MacBook-Pro.local>
2017-11-07 21:34:13 -0800

    implemented alert ui for convert button and label update

---
+++ Conversion Calculator/Converter.swift
@@ -0,0 +1,15 @@
+//
+//  Converter.swift
+//  Conversion Calculator
+//
+//  Created by Andy Hine on 11/7/17.
+//  Copyright © 2017 Andy Hine. All rights reserved.
+//
+
+import Foundation
+
+struct Converter{
+    var label:String
+    var inputUnit:String
+    var outputUnit:String
+}
--- Conversion Calculator/ConverterViewController.swift
+++ Conversion Calculator/ConverterViewController.swift
@@ -9,11 +9,40 @@
 import UIKit
 
 class ConverterViewController: UIViewController {
+    
+    let converters:[Converter] = [
+        Converter(label: "fahrenheit to celcius", inputUnit: "°F", outputUnit: "°C"),
+        Converter(label: "celcius to fahrenheit", inputUnit: "°C", outputUnit: "°F"),
+        Converter(label: "miles to kilometers", inputUnit: "mi", outputUnit: "km"),
+        Converter(label: "kilometers to miles", inputUnit: "km", outputUnit: "mi")
+    ]
 
+    @IBOutlet weak var outputDisplay: UITextField!
+    @IBOutlet weak var inputDisplay: UITextField!
+    
+    @IBAction func convertTapped(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: nil, message: "Choose Converter", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        for converter in converters{
+            alert.addAction(UIAlertAction(title: converter.label, style: UIAlertActionStyle.default, handler: {
+                (alertAction) -> Void in
+                self.inputDisplay.text = converter.inputUnit
+                self.outputDisplay.text = converter.outputUnit
+            }))
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+       
+        
+        let defaultConverter = converters[0]
+        
+        inputDisplay.text = defaultConverter.inputUnit
+        outputDisplay.text = defaultConverter.outputUnit
     }
 
     override func didReceiveMemoryWarning() {
